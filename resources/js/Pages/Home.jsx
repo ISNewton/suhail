ba0a8760a865d2017e18f6ae9ba60ccfdbdf70d5
Ashraf Alhaj <ashrfalhag.11@gmail.com>
2024-05-18 23:13:44 +0200

    feat:pricing & our message sections

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,12 +2,16 @@ import { Button } from "flowbite-react";
 import Navbar from "../Layout/Navbar.tsx";
 import Hero from "../Components/Hero.tsx";
 import StepsSection from "../Components/StepsSection.tsx";
+import PricingSection from "../Components/PricingSection.tsx";
+import OurMessage from "../Components/OurMessage.tsx";
 export default function Home() {
     return (
         <div>
             <Navbar />
             <Hero />
             <StepsSection />
+            <PricingSection />
+            <OurMessage />
 
         </div>
 )
